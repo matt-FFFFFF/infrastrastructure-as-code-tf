25db63f23b4d16014af6af7c581120a67bd6cd70
Matt White <matt.ffffff@gmail.com>
2019-01-22 18:55:48 +0000

    Change pipeline to use default azurerm provider environment variables. e.g. ARM_*
    Presiously we used specific variables resulting in more code.

--- tf-files/main.tf
+++ tf-files/main.tf
@@ -4,18 +4,7 @@ terraform {
   backend "azurerm" {}
 }
 
-variable "AZURERM_SUBSCRIPTION_ID" {}
-variable "AZURERM_CLIENT_ID" {}
-variable "AZURERM_CLIENT_SECRET" {}
-variable "AZURERM_TENANT_ID" {}
-
-provider "azurerm" {
-  version         = "~> 1.15"
-  subscription_id = "${var.AZURERM_SUBSCRIPTION_ID}"
-  client_id       = "${var.AZURERM_CLIENT_ID}"
-  client_secret   = "${var.AZURERM_CLIENT_SECRET}"
-  tenant_id       = "${var.AZURERM_TENANT_ID}"
-}
+provider "azurerm" {}
 
 resource "azurerm_resource_group" "testrg" {
   count    = "1"
