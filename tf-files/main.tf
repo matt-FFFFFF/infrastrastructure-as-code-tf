2d2ec8b5f34fcb13c0797acf8af305131f6bfa0c
Matt White <matt.ffffff@gmail.com>
2019-01-22 19:31:48 +0000

    Revert to declared variables for azurerm provider

--- tf-files/main.tf
+++ tf-files/main.tf
@@ -4,8 +4,17 @@ terraform {
   backend "azurerm" {}
 }
 
+variable "AZURERM_CLIENT_ID" {}
+variable "AZURERM_SUBSCRIPTION_ID" {}
+variable "AZURERM_CLIENT_SECRET" {}
+variable "AZURERM_TENANT_ID" {}
+
 provider "azurerm" {
-  version = "~> 1.21"
+  version         = "~> 1.21"
+  client_id       = "${var.AZURERM_CLIENT_ID}"
+  subscription_id = "${var.AZURERM_SUBSCRIPTION_ID}"
+  tenant_id       = "${var.AZURERM_TENANT_ID}"
+  client_secret   = "${var.AZURERM_CLIENT_SECRET}"
 }
 
 resource "azurerm_resource_group" "testrg" {
