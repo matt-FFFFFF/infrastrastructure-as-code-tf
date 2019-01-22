2fa0efd41eb0d2295d954be0ea1064aea005b860
Matt White <matt.ffffff@gmail.com>
2019-01-22 19:09:48 +0000

    Fix required_version in azurerm provider.
    Change to 'version'

--- tf-files/main.tf
+++ tf-files/main.tf
@@ -1,29 +1,9 @@
 terraform {
-  required_version = "~>0.11.10"
+  required_version = "~> 0.11.10"
 
   backend "azurerm" {}
 }
 
 provider "azurerm" {
-  required_version = "~>1.21"
-}
-
-resource "azurerm_resource_group" "testrg" {
-  count    = "1"
-  name     = "testrg"
-  location = "westeurope"
-
-  tags {
-    provisioned_by = "terraform"
-  }
-}
-
-resource "azurerm_resource_group" "testrg2" {
-  count    = "1"
-  name     = "testrg2"
-  location = "westeurope"
-
-  tags {
-    provisioned_by = "terraform"
-  }
+  version = "~> 1.21"
 }
